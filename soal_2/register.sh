4cf7517693e326871c691382cd7779a202094e15
farelrafli <152679404+farelrafli@users.noreply.github.com>
2025-03-20 22:57:08 +0700

    Update register.sh

--- soal_2/register.sh
+++ soal_2/register.sh
@@ -20,6 +20,7 @@ read -p "Masukkan Email: " email
 read -p "Masukkan Username: " username
 read -s -p "Masukkan Password: " password
 echo ""
+
 # Validasi email
 if ! [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
     echo "Email tidak valid!"
@@ -40,6 +41,7 @@ fi
 
 # Hash password dengan SHA-256 + salt
 hashed_password=$(echo -n "$password$SALT" | sha256sum | awk '{print $1}')
+
 # Simpan ke database
 echo "$email,$username,$hashed_password" >> "$DB_FILE"
 
